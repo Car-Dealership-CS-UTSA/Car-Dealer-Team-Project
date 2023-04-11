858bcbe20ed4adc2bc907b1982034572946db018
shob4 <JonathonBonneau@gmail.com>
2023-04-11 14:19:34 -0500

    not sure how to return con.connect as message in app.get

---
+++ frontend/src/Quotes.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+function Quotes() {
+  const [text, setText] = useState("");
+
+// requesting that get defined in server.js
+function getText() {
+  axios.get("http://localhost:5000/", { crossdomain: true
+  }).then(response => {
+    // setting text with setText
+    setText(response.data.text);
+  });
+}
+
+return (
+    <div>
+      <button onClick={getText}>
+        hello
+      </button>
+      // putting text on screen
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+export default Quotes;
--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
 // packages needed for node.js to connect
 const mysql = require('mysql');
 // stuff for connecting to javasccript front end
-//const prompt = require('prompt-sync');
-//const express = require('express');
-//const cors = require('cors');
+const prompt = require('prompt-sync');
+const express = require('express');
+const cors = require('cors');
 
 // express creates express application
-//const app = express();
+const app = express();
 // allow cross-origin requests. idk what that means
-//app.use(cors());
+app.use(cors());
 // parse incoming requests with JSON
-//app.use(express.json());
+app.use(express.json());
 
 // creates a GET route
 // two arguments: path of endpoint which is just /message
 // callback which can be middleware function or series/array 
 // of middleware functions
-//app.get('/message', (req, res) => {
-//  // returning json object, so use res.json()
-//  res.sjon({message: "hello from server!"});
-//});
-
-// starts server
-//app.listen(8000, () => {
-//  console.log('server running on port 8000');
-//});
+app.get('/message', (req, res) => {
+  // returning json object, so use res.json()
+  answer = connect();
+  res.json({message: string});
+});
 
 // this part actually connects to database
 // sql needs to be connected to front end
@@ -37,15 +33,21 @@ var con = mysql.createConnection({
 				database: "helloworld"
 });
 
-con.connect(function(err) {
-				// throws error if not connected
-				if (err) throw err;
-				console.log("connected!");
-				// query database
-				con.query(sql, function (err, result) {
-								if (err) throw err;
-								console.log("Result: " + result);
-				});
+// defunct first prototype
+// con.connect(function(err) {
+// 				// throws error if not connected
+// 				if (err) throw err;
+// 				console.log("connected!");
+// 				// query database
+// 				con.query(sql, function (err, result) {
+// 								if (err) throw err;
+// 								console.log("Result: " + result);
+// 				});
+// });
+
+// starts server
+app.listen(8000, () => {
+  console.log('server running on port 8000');
 });
 
 // all of these are function calls
@@ -67,4 +69,3 @@ con.connect(function(err) {
 								console.log("ending connection");
 				});
 });
-*/
