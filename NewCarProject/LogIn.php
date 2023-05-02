fdcf8fa2a9e6d0cd931d0435e6a78bdacb70f731
Ayrton Bello <59799942+aaybe@users.noreply.github.com>
2023-05-02 11:13:02 -0500

    Update LogIn.php

--- NewCarProject/LogIn.php
+++ NewCarProject/LogIn.php
@@ -28,7 +28,7 @@ else{
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="app.css">
@@ -144,7 +144,7 @@ else{
 
 ?>
       </div>
-      <div align='right'>
+      <div class="text-center" align='right'>
         <a onclick='window.location.href = "CreateAccount.html";' class='button'> Create Account</a>
       </div>
       <script>
